94431cd2c240294f6f98f7a383753e1654a6cffb
chmod777Alex <59450257+alexegorov1@users.noreply.github.com>
2025-05-08 11:12:42 -0400

    Update pe_inspector.py

--- collector/pe_inspector.py
+++ collector/pe_inspector.py
@@ -2,10 +2,6 @@ import pefile
 import os
 
 def is_pe_file(file_path: str) -> bool:
-    """
-    Returns True if the given file is a valid Portable Executable (PE) file.
-    If the file is not a PE or can't be parsed, returns False.
-    """
     try:
         if not os.path.isfile(file_path):
             return False
