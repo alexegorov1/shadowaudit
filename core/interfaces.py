8acc9d25c284b06ff125dc7a0add1bb2244df5c5
chmod777Alex <59450257+alexegorov1@users.noreply.github.com>
2025-06-16 13:51:04 -0400

    Update interfaces.py

--- core/interfaces.py
+++ core/interfaces.py
@@ -6,3 +6,8 @@ class BaseCollector(ABC):
     def collect(self) -> List[Dict]:
         """Collects and returns a list of artifact dictionaries."""
         pass
+
+    @abstractmethod
+    def get_name(self) -> str:
+        """Returns the unique name of the collector module."""
+        pass
