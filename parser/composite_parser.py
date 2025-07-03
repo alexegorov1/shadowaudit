fc86b2ccb94c994ef728094d5f4b3b79390ae522
chmod777Alex <59450257+alexegorov1@users.noreply.github.com>
2025-07-03 00:35:10 -0400

    Update composite_parser.py

--- parser/composite_parser.py
+++ parser/composite_parser.py
@@ -11,7 +11,6 @@ class CompositeParser(BaseParser):
     def get_name(self) -> str:
         return self._name
 
-
     def add_parser(self, parser: BaseParser) -> None:
         pid = id(parser)
         if pid in self._parser_ids:
